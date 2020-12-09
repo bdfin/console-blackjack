f5449e4164687a63baee0b9b9704ac8b6b5f3021
Beau <beau@vouch.co.uk>
2020-12-09 22:04:04 +0000

    Add initial Deck tests.

---
+++ console-blackjack.tests/DeckTests.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleBlackjack.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace ConsoleBlackjack.Tests
+{
+    [TestClass]
+    public class DeckTests
+    {
+        [TestMethod]
+        public void HasAllCards()
+        {
+            var deck = new Deck();
+            deck.Build();
+
+            Assert.IsTrue(deck.Cards.Count is 52);
+        }
+
+        [TestMethod]
+        public void HasBeenShuffled()
+        {
+            var orderedDeck = new Deck();
+            orderedDeck.Build();
+
+            var shuffledDeck = new Deck();
+            shuffledDeck.Build();
+            shuffledDeck.Shuffle();
+
+            Assert.IsFalse(orderedDeck.Cards.SequenceEqual(shuffledDeck.Cards));
+        }
+    }
+}
