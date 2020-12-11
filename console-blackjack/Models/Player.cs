bcba9f3d2150d06308d70c064dcd97419122f534
Beau <beau@vouch.co.uk>
2020-12-11 15:31:18 +0000

    Add player model.

---
+++ console-blackjack/Models/Player.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleBlackjack.Common;
+using ConsoleBlackjack.Helpers;
+using System.Collections.Generic;
+
+namespace ConsoleBlackjack.Models
+{
+    public class Player
+    {
+        public List<Card> Cards { get; set; }
+        public decimal Chips { get; set; }
+        public int HandsWon { get; set; }
+
+        public Player()
+        {
+            Chips = (decimal)StartingChips.Low;
+            Cards = new List<Card>();
+            HandsWon = 0;
+        }
+
+        public void Bet(decimal amount)
+        {
+            if (amount > Chips)
+            {
+                ConsoleExtention.WriteErrorMessage(BettingMessages.InsufficientChips);
+            }
+            else
+            {
+                Chips -= amount;
+            }
+        }
+    }
+}
